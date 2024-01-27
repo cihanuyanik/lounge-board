eee7b92b1ca9a886c9270686186bd00fa7a364e6
Cihan Uyanik <cihanuyanik34@gmail.com>
2024-01-27 14:30:52 +0100

    Create config.ts

---
+++ src/api/config.ts
@@ -0,0 +1,11 @@
+// Web app's Firebase configuration
+// noinspection SpellCheckingInspection
+export const firebaseConfig = {
+  apiKey: "AIzaSyAA6DugSc7GnRGFwQcjUOfNB6JenVFEUV4",
+  authDomain: "digital-health-lounge-board.firebaseapp.com",
+  projectId: "digital-health-lounge-board",
+  storageBucket: "digital-health-lounge-board.appspot.com",
+  messagingSenderId: "821565731199",
+  appId: "1:821565731199:web:fbb92e5be8456d1f2c6a4a",
+  measurementId: "G-0M73WZQDDB",
+};
