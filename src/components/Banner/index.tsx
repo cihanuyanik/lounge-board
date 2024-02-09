31cd9ad535d47350774c05c954b12392da82b0fe
Cihan Uyanik <cihanuyanik34@gmail.com>
2024-02-09 10:57:31 +0100

    Research group banner image transition with active index

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -24,58 +24,88 @@ export default function (props: BannerProps) {
 
   return (
     <Row class={"banner w-full gap-2"}>
-      <Img
-        src={DTULogo}
-        class={"dtu-logo"}
-        onClick={() => {
-          !isAdmin() ? navigate("/admin") : navigate("/");
-        }}
-      />
-
-      <Show when={props.user}>
-        <Row class={"user"}>
-          <Row class={"avatar"}>
-            <Img src={props.user?.photoURL || AvatarPlaceholder}></Img>
-          </Row>
-          <Column>
-            <p>{"Welcome"}</p>
-            <p>{props.user?.displayName}</p>
-            <Row class={"gap-1"}>
-              <Button
-                popupContent={
-                  "Sign out from admin account and go to index page"
-                }
-                onClick={async () => {
-                  await Executor.run(() => API.AuthService.signOut(), {
-                    busyDialogMessage: "Signing out...",
-                    postAction: () => navigate("/", {}),
-                  });
-                }}
-              >
-                Out
-                <Logout />
-              </Button>
-              <Button
-                popupContent={"Go to profile update page"}
-                onClick={() => {
-                  navigate("/profile");
-                }}
-              >
-                <Settings />
-              </Button>
-            </Row>
-          </Column>
-        </Row>
-      </Show>
+      <Logo />
+      <AdminUser user={props.user} />
       <Row class={"name"}>{props.title}</Row>
       <Row class={"flex-1"} />
-      <Show when={props.showResearchGroups}>
-        <Row class={"group-images gap-4"}>
-          <For each={researchGroups.ids}>
-            {(id) => <Img src={researchGroups.entities[id].bannerImage} />}
-          </For>
-        </Row>
-      </Show>
+      <ResearchGroups show={props.showResearchGroups} />
     </Row>
+  );
+}
+
+function Logo() {
+  const navigate = useNavigate();
+  const { isAdmin } = useAppContext();
+
+  return (
+    <Img
+      src={DTULogo}
+      class={"dtu-logo"}
+      onClick={() => {
+        !isAdmin() ? navigate("/admin") : navigate("/");
+      }}
+    />
+  );
+}
+
+function AdminUser(props: { user?: User }) {
+  const navigate = useNavigate();
+  const { Executor, API } = useAppContext();
+
+  return (
+    <Show when={props.user}>
+      <Row class={"user"}>
+        <Row class={"avatar"}>
+          <Img src={props.user?.photoURL || AvatarPlaceholder}></Img>
+        </Row>
+        <Column>
+          <p>{"Welcome"}</p>
+          <p>{props.user?.displayName}</p>
+          <Row class={"gap-1"}>
+            <Button
+              popupContent={"Sign out from admin account and go to index page"}
+              onClick={async () => {
+                await Executor.run(() => API.AuthService.signOut(), {
+                  busyDialogMessage: "Signing out...",
+                  postAction: () => navigate("/", {}),
+                });
+              }}
+            >
+              Out
+              <Logout />
+            </Button>
+            <Button
+              popupContent={"Go to profile update page"}
+              onClick={() => {
+                navigate("/profile");
+              }}
+            >
+              <Settings />
+            </Button>
+          </Row>
+        </Column>
+      </Row>
+    </Show>
+  );
+}
+
+function ResearchGroups(props: { show?: boolean }) {
+  const { researchGroups } = useAppContext();
+
+  return (
+    <Show when={props.show}>
+      <Row class={"group-images gap-4"}>
+        <For each={researchGroups.ids}>
+          {(id) => (
+            <Img
+              src={researchGroups.entities[id].bannerImage}
+              classList={{
+                active: researchGroups.active.id === id,
+              }}
+            />
+          )}
+        </For>
+      </Row>
+    </Show>
   );
 }
